aa50203341c31b61ef31ab00305eef576a525d7d
Chatchai <hydeqoo@hotmail.com>
2015-09-13 15:36:35 +0700

    Add app.js to result.php

--- result.php
+++ result.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="js/app.js"></script>
 	<style type="text/css">
 		body {
 			margin: 10px;
@@ -21,27 +21,12 @@
 			width: 50px;
 		}
 	</style>
+	<script type="text/javascript">
+		ShowList();
+	</script>
 <body>
-	<h1>List of Item</h1>
+	<h1>List of Items</h1>
 	<div id ="item">
-		<table id="tab">
-			<thead>
-				<tr>
-					<th >No</th>
-					<th>Description</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>1</td>
-					<td>Book 1</td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td>Book 2</td>
-				</tr>
-			</tbody>
-		</table>
 	</div>
 	<h2 id="result"></h2>
 	<script type="text/javascript">
