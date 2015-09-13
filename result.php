02cbeaf31b49e8d28285ae39e1dc4c1a88c408bd
Chatchai <hydeqoo@hotmail.com>
2015-09-13 14:37:37 +0700

    Add html basic structure in result.php

---
+++ result.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Grean Press - Checkout</title>
+</head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<body>
+	<h1>List of Item</h1>
+	<table id="item" class="table table-condensed" style="b">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Description</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>Jackson</td>
+			</tr>
+		</tbody>
+	</table>
+	<h2 id="result"></h2>
+</body>
+</html>
