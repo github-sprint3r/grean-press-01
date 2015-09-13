fe6eb8f7318bb30bc1d920fe35d0b2932bb2f035
Pichaya Morimoto <pich4ya@googlemail.com>
2015-09-13 14:32:48 +0700

    ini check vip function

--- src/checkout.php
+++ src/checkout.php
@@ -1,11 +1,23 @@
 <?php
 
 class CheckOut{
-
+	/*
+	Input: 
+		- ???
+	}
+	*/
 	function __construct(){
 
 	}
-	
+
+
+	function checkUserVIP($uid){
+		if($uid == 0){
+			return json_encode(["vip" => TRUE]);
+		}
+		return json_encode(["vip" => FALSE]);
+	}
+
 }
 
 ?>
