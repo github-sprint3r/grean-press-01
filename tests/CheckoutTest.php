57b1da75a96ab15ce642f95e5297ba4f18d7e126
Kanitkorn S <k.sujautra@gmail.com>
2015-09-13 15:32:04 +0700

    Close #1 - Pass tests Test with - testVIPBuySixBookShippingCostShouldBeFree - testVIPBuySixBookShippingResultShouldBeFree

--- tests/CheckoutTest.php
+++ tests/CheckoutTest.php
@@ -7,14 +7,26 @@ class CheckoutTest extends PHPUnit_Framework_TestCase
     private $freeResultText = 'ฟรีค่าจัดส่งสินค้า';
     private $notFreeResultText = 'เสียค่าจัดส่งสินค้า';
 
-    public function testVIPBuyFiveBookShippingCostShouldBeFree()
+    public function testVIPBuySixBookShippingCostShouldBeFree()
     {
         // 5 Books
         // 1 CD
         // VIP = 1
-        $checkout = new CheckOut(5, 1, 1);
+        $checkout = new CheckOut(6, 1, 1);
 
         $this->assertEquals($this->freeResultText, $checkout->getText());
+    }
+
+    public function testVIPBuySixBookShippingResultShouldBeFree()
+    {
+        // 5 Books
+        // 1 CD
+        // VIP = 1
+        $checkout = new CheckOut(6, 1, 1);
+
+        $checkoutResult = json_decode($checkout->checkOutResult());
+
+        $this->assertEquals($this->freeResultText, $checkoutResult->result_text);
     }
 
     public function testVIPBuyFourBookShippingCostShouldNotBeFree()
